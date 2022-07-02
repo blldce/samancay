9c969998106d4848474018775e1ab3f36004f3fa
ceyhun <reaur@pop-os.localdomain>
2022-07-03 02:03:17 +0300

    interrupt decriptor table

---
+++ src/memory/memory.c
@@ -0,0 +1,27 @@
+#include <memory.h>
+
+void *memset(void(*addr), int value, size_t size)
+{
+    char(*c_ptr) = (char(*))addr;
+
+    int index = 0;
+    while (index < size)
+    {
+        c_ptr[index] = (char)value;
+        index++;
+    }
+
+    return addr;
+}
+
+/*
+void* memset(void* ptr, int c, size_t size)
+{
+    char* c_ptr = (char*) ptr;
+    for (int i = 0; i < size; i++)
+    {
+        c_ptr[i] = (char) c;
+    }
+    return ptr;
+} 
+*/
