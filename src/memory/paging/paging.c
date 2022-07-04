a5075adb7b9aa7c26daaa0e0079f1a7faf89d116
blldce <blldce@gmail.com>
2022-07-04 21:45:47 +0300

    page table init

--- src/memory/paging/paging.c
+++ src/memory/paging/paging.c
@@ -9,25 +9,30 @@ struct paging_4gb *new_paging_4gb(uint8_t flags)
     // page table base address
     int page_table_offset = 0;
 
-    // initialize buffer
+    // initialize directory buffer
     int directory_entry_index = 0;
     while (directory_entry_index < TOTAL_ENTRIES_PER_TABLE)
     {
         // allocate a buffer (1024) for page table entry
         uint32_t(*page_table_entry_buffer) = kzalloc(sizeof(uint32_t) * TOTAL_ENTRIES_PER_TABLE);
-        // apply
 
+        // initialize page table buffer
         int page_table_entry_index = 0;
-        // page table initialization
         while (page_table_entry_index < TOTAL_ENTRIES_PER_TABLE)
         {
-            // page table entry address mapping to physical address 
-            // Example : Physical address = (0 +(0 * 4096)) = 0, (0 +(1 * 4096)) = 4096 ... 
+            // page table entry address mapping to physical address
+            // Example : Physical address = (0 +(0 * 4096)) = 0, (0 +(1 * 4096)) = 4096 ...
             page_table_entry_buffer[page_table_entry_index] = (page_table_offset + (page_table_entry_index * ENTRY_SIZE_IN_BYTES)) | flags;
             page_table_entry_index++;
         }
         // move next page table base address
         page_table_offset += (TOTAL_ENTRIES_PER_TABLE * ENTRY_SIZE_IN_BYTES);
+
+        directory_buffer[directory_entry_index] = (*(uint32_t(*))page_table_entry_buffer) | flags | PAGING_IS_WRITABLE;
         directory_entry_index++;
     }
+
+    struct paging_4gb(*chunk_4gb) = kzalloc(sizeof(struct paging_4gb));
+    chunk_4gb->directory_entry = directory_buffer;
+    return chunk_4gb;
 }
