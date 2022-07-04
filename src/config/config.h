767542244a2d74c29efe5519290c903a96b4d047
blldce <blldce@gmail.com>
2022-07-04 08:37:47 +0300

    bug fixed

--- src/config/config.h
+++ src/config/config.h
@@ -28,11 +28,11 @@ Heap Base Address ........
 */
 
 // RAM -- free for use (https://wiki.osdev.org/Memory_Map_(x86))
-#define HEAP_BASE_ADDRESS 0x01000000
+#define HEAP_BASE_ADDRESS 0x01000000 
 
 // 1024 bytes * 1024 = 1048576 (1MB), => 1048576 * 100 = 104857600 (100 MB)
 // Kernel requires at least 100MB for heap usage
-#define TOTAL_HEAP_SIZE_IN_BYTES 104857600
+#define TOTAL_HEAP_SIZE_IN_BYTES 104857600 
 
 // Each heap entry is 4096 bytes aligned. For example: malloc(5000), => 8192 bytes allocated (2 entries)
 #define HEAP_ENTRY_SIZE_IN_BYTES 4096
